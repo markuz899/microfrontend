2549b15539709534be6c41be4e2cf6babf6d0829
markuz899 <markuz.89@hotmail.it>
2024-12-12 22:26:57 +0100

    [feat] - global style

--- remote-a/src/App.jsx
+++ remote-a/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import AppRoutes from "./Routes";
 import { ThemeProvider } from "styled-components";
 import { ThemeProvider as ThemeTenantProvider } from "@nyp/tenantuikit";
+import { GlobalStyle } from "./theme/global-styles";
 
 function App({ title, theme }) {
   return (
     <ThemeTenantProvider theme={theme}>
       <ThemeProvider theme={theme}>
         <AppRoutes title={title} theme={theme} />
+        <GlobalStyle />
         <div id="root-modal"></div>
         <div id="root-tooltip"></div>
       </ThemeProvider>
