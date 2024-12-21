ac7256be41c18482af3f2ba9a546e2d30b1649e9
markuz899 <markuz.89@hotmail.it>
2024-12-21 15:59:07 +0100

    [fix] - color scheme

--- host/src/navbar.tsx
+++ host/src/navbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div style={{ display: "flex" }}>
+    <div style={{ display: "flex", gap: "20px" }}>
       <ul>
         <li>Host</li>
         <li>
